9ac6aed395573e7d449448f58acdc7d8cca297d3
saurabh50183@gmail.com <saurabh saneja>
2023-08-25 17:23:32 +0530

    minor

--- src/pages/Deal/Jobs/CompanyProfile.js
+++ src/pages/Deal/Jobs/CompanyProfile.js
@@ -86,15 +86,15 @@ const CompanyProfile = (props) => {
   const [profileData, setProfileData] = useState({});
   const [tabs, setTabs] = useState([
     {
-      id: '1',
-      name: 'About Us'
+      id: "1",
+      name: "About Us",
     },
     {
-      id: '2',
-      name: 'Jobs'
+      id: "2",
+      name: "Jobs",
     },
   ]);
-  const [selectedTab, setSelectedTab] = useState('1')
+  const [selectedTab, setSelectedTab] = useState("1");
 
   useEffect(() => {
     console.log("userdetaile.token", userdetaile);
@@ -119,11 +119,11 @@ const CompanyProfile = (props) => {
     }
   };
   const changeSelectedTab = (id) => {
-    if(selectedTab === id){
-      return
+    if (selectedTab === id) {
+      return;
     }
-    setSelectedTab(id)
-  }
+    setSelectedTab(id);
+  };
   return (
     <SafeAreaView style={styles.safeView}>
       <ScrollView
@@ -131,21 +131,43 @@ const CompanyProfile = (props) => {
         style={{ width: "100%" }}
         contentContainerStyle={styles.mainView}
       >
-       <CompanyHeader/>
-       <View style={styles.container}>
-        <View style={styles.iconBtnRow}>
-            <IconButton text='Post Jobs' icon={require('./assets/images/jobs-add.png')} onPress={()=>{}} />
-            <IconButton text='Visit website' icon={require('./assets/images/jobs-visit.png')} onPress={()=>{}} style={{marginLeft: 20}} />
+        <CompanyHeader />
+        <View style={styles.container}>
+          <View style={styles.iconBtnRow}>
+            <IconButton
+              text="Post Jobs"
+              icon={require("./assets/images/jobs-add.png")}
+              onPress={() => {}}
+            />
+            <IconButton
+              text="Visit website"
+              icon={require("./assets/images/jobs-visit.png")}
+              onPress={() => {}}
+              style={{ marginLeft: 20 }}
+            />
           </View>
 
-        <View style={styles.tabsRow} >
-          {tabs?.map(item => 
-            <TouchableOpacity onPress={()=>changeSelectedTab(item.id)} style={[styles.tabView, selectedTab !== item.id ? {backgroundColor: 'white'}: null]} >
-              <Text style={[styles.tabTxt, selectedTab !== item.id ? {color: 'black'}: null]}>{item.name}</Text>
-            </TouchableOpacity>
-          )}
-        </View>   
-        </View> 
+          <View style={styles.tabsRow}>
+            {tabs?.map((item) => (
+              <TouchableOpacity
+                onPress={() => changeSelectedTab(item.id)}
+                style={[
+                  styles.tabView,
+                  selectedTab !== item.id ? { backgroundColor: "white" } : null,
+                ]}
+              >
+                <Text
+                  style={[
+                    styles.tabTxt,
+                    selectedTab !== item.id ? { color: "black" } : null,
+                  ]}
+                >
+                  {item.name}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
       </ScrollView>
       {loading ? <Loader /> : null}
       {My_Alert ? (
@@ -161,42 +183,45 @@ const CompanyProfile = (props) => {
 };
 export default CompanyProfile;
 
-const CompanyHeader = ({goBack}) => {
+const CompanyHeader = ({ goBack }) => {
   return (
     <View style={styles.hdrContainer}>
       <View style={styles.hdrTopRow}>
-        <TouchableOpacity onPress={goBack} >
-            <Image source={require('./assets/images/arrow-left.png')} />
+        <TouchableOpacity onPress={goBack}>
+          <Image source={require("./assets/images/arrow-left.png")} />
         </TouchableOpacity>
-        <TouchableOpacity onPress={goBack} >
-            <Image source={require('./assets/images/bookmark-white.png')} />
+        <TouchableOpacity onPress={goBack}>
+          <Image source={require("./assets/images/bookmark-white.png")} />
         </TouchableOpacity>
       </View>
       <View style={styles.iconBg}>
-        <Image source={require('./assets/images/google-icon.png')} style={{width: 54.6, height: 54.6}} />
+        <Image
+          source={require("./assets/images/google-icon.png")}
+          style={{ width: 54.6, height: 54.6 }}
+        />
       </View>
       <Text style={styles.compName}>Google</Text>
       <View style={styles.numEmpRow}>
         <View style={styles.numEmpView}>
           <Text style={styles.numEmpText}>1,001-5,000 employees</Text>
         </View>
-        <View style={[styles.numEmpView, {marginLeft: 5}]}>
+        <View style={[styles.numEmpView, { marginLeft: 5 }]}>
           <Text style={styles.numEmpText}>146 applicants.</Text>
         </View>
       </View>
       <Text style={styles.lctnTxt}>Seattle, USA</Text>
     </View>
-  )
-}
+  );
+};
 
-const IconButton = ({icon, text, onPress, style = {}}) => {
+const IconButton = ({ icon, text, onPress, style = {} }) => {
   return (
-    <TouchableOpacity onPress={onPress} style={[styles.iconBtn, style]} >
+    <TouchableOpacity onPress={onPress} style={[styles.iconBtn, style]}>
       <Image source={icon} />
       <Text style={styles.iconTxt}>{text}</Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   safeView: {
@@ -212,107 +237,117 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     marginTop: -30,
   },
-  hdrContainer:{
-    backgroundColor: '#0089CF',
-    width: '100%',
+  hdrContainer: {
+    backgroundColor: "#0089CF",
+    width: "100%",
     // height: 100,
     paddingTop: 16.6,
     paddingHorizontal: 20,
     paddingBottom: 30,
     borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30  
+    borderBottomRightRadius: 30,
   },
-  hdrTopRow:{
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-between',
+  hdrTopRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   iconBg: {
     height: 84,
     width: 84,
-    borderRadius: 84/2,
+    borderRadius: 84 / 2,
     backgroundColor: "rgba(255, 255, 255, 0.88)",
     justifyContent: "center",
     alignItems: "center",
-    alignSelf:'center'
+    alignSelf: "center",
   },
-  compName:{
-    color: 'white',
+  compName: {
+    color: "white",
     fontSize: 18,
-    fontFamily: '700',
-    textAlign:'center',
-    marginTop: 16
+    fontWeight: "700",
+    textAlign: "center",
+    marginTop: 16,
   },
   numEmpRow: {
-    flexDirection:'row',
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 11
+    marginTop: 11,
   },
-  numEmpView:{
-    backgroundColor:'rgba(255, 255, 255, 0.15)',
+  numEmpView: {
+    backgroundColor: "rgba(255, 255, 255, 0.15)",
     paddingVertical: 4,
     paddingHorizontal: 16,
-    borderRadius: 65
+    borderRadius: 65,
   },
-  numEmpText:{
-    color: 'white',
+  numEmpText: {
+    color: "white",
     fontSize: 11,
-    fontFamily: '400'
+    fontWeight: "400",
   },
-  lctnTxt:{
-    color: 'white',
+  lctnTxt: {
+    color: "white",
     fontSize: 16,
-    fontFamily: '600',
-    textAlign:'center',
-    marginTop: 8
+    fontWeight: "600",
+    textAlign: "center",
+    marginTop: 8,
   },
-  iconBtnRow:{
+  iconBtnRow: {
     // paddingHorizontal: 24,
     marginTop: 21,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  iconBtn:{
-    backgroundColor:'rgba(0, 137, 207, 0.12)',
-    borderRadius:5,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'center',
-    paddingVertical: 10,
-    width: (dimensions.SCREEN_WIDTH -  (24 * 2 + 20)) / 2
-  },
-  iconTxt:{
-    color:'#0089CF',
-    fontSize:12,
-    fontWeight:'400',
-    marginLeft: 10
-  },
-  container:{
-    paddingHorizontal: 24
-  },
-  tabsRow:{
-    marginTop: 18,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-between',
-    backgroundColor:'white',
-    borderRadius:10,
-    paddingHorizontal: 9,
-    paddingVertical:7
-  },
-  tabView:{
-    paddingVertical: 10,
-    backgroundColor:'#0089CF',
-    alignItems:'center',
-    justifyContent:'center',
+  iconBtn: {
+    backgroundColor: "rgba(0, 137, 207, 0.12)",
     borderRadius: 5,
-    width: (dimensions.SCREEN_WIDTH -  (24 * 2 + 40)) / 2
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 10,
+    width: (dimensions.SCREEN_WIDTH - (24 * 2 + 20)) / 2,
   },
-  tabTxt:{
-    color:'white',
+  iconTxt: {
+    color: "#0089CF",
+    fontSize: 12,
+    fontWeight: "400",
+    marginLeft: 10,
+  },
+  container: {
+    paddingHorizontal: 24,
+  },
+  tabsRow: {
+    marginTop: 18,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    borderRadius: 10,
+    paddingHorizontal: 9,
+    paddingVertical: 7,
+  },
+  tabView: {
+    paddingVertical: 10,
+    backgroundColor: "#0089CF",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+    width: (dimensions.SCREEN_WIDTH - (24 * 2 + 40)) / 2,
+  },
+  tabTxt: {
+    color: "white",
     fontSize: 14,
-    fontWeight: '700'
-  }
+    fontWeight: "700",
+  },
+  heading: {
+    color: "#150B3D",
+    fontSize: 14,
+    fontWeight: "700",
+  },
+  valueStyle: {
+    color: "#524B6B",
+    fontSize: 12,
+    fontWeight: "400",
+  },
 });
